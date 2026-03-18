cb79ce85cace306d4600d6db892f0ea826e1d917
Chandan Bhadrala <C.Bhadrala88@gmail.com>
2026-03-18 18:10:33 +0530

    Completed Tweet section. Now, onto the RightSidebar.

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -5,7 +5,7 @@ import { IoFlagOutline } from "react-icons/io5";
 
 const CreatePost = () => {
   return (
-    <div>
+    <div className="border-b border-amber-200 pb-4">
       {/* Top-section */}
       <div className="flex items-center justify-around font-semibold text-xl border-b border-amber-200">
         <div className="hover:cursor-pointer hover:bg-amber-200 w-full text-center py-4 ">
@@ -29,21 +29,21 @@ const CreatePost = () => {
         <div className="ml-4 flex-1 ">
           <textarea
             placeholder="What's happening"
-            className="border-b border-amber-200 outline-none w-full  resize-none h-40"
+            className="border-b border-amber-200 outline-none w-full  resize-none h-30"
           />
         </div>
       </div>
 
       {/* Post-Button-section */}
       <div className="flex justify-between px-8 items-center">
-        <div className="flex justify-around gap-x-4">
+        <div className="flex justify-around gap-x-4 ml-8">
           <CiImageOn size="24px"/>
           <MdOutlineGifBox size="24px"/>
           <VscSmiley size="24px"/>
           <IoFlagOutline size="24px"/>
         </div>
 
-        <button className="bg-black text-lg text-white font-semibold py-2 rounded-4xl px-8">Post</button>
+        <button className="bg-black text-lg text-white font-semibold py-2 rounded-4xl px-8 cursor-pointer">Post</button>
       </div>
     </div>
   );
--- client/src/components/Tweet.jsx
+++ client/src/components/Tweet.jsx
@@ -1,6 +1,64 @@
+import { IoChatbubbleOutline } from "react-icons/io5";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
+import { CiBookmark } from "react-icons/ci";
+import { MdOutlineFileUpload } from "react-icons/md";
+import { IoStatsChartOutline } from "react-icons/io5";
+
 const Tweet = () => {
   return (
-    <div>Tweet</div>
+    <div className="p-4 flex gap-4 border-b border-amber-200">
+      <img
+        src="https://i.pravatar.cc/150"
+        alt="avatar"
+        className="w-10 h-10 rounded-full "
+      />
+
+      <div>
+        <h1 className="font-semibold">
+          Chandan Bhadrala{" "}
+          <span className="text-gray-400 font-normal">@ChandanBhadrala 1m</span>
+        </h1>
+        <h2 className="text-gray-400 font-normal">Replying to @Kirat_tw</h2>
+        <p>
+          I'm onto the learning phase so, I'm all hands on the keyboard with VS
+          code old school type.
+        </p>
+
+        {/* Bottom Action Images */}
+        <div className="flex justify-between pt-4 items-center">
+          <div className="flex flex-8 justify-around">
+            <div className="flex gap-1 items-center cursor-pointer hover:bg-amber-200 rounded-4xl p-2">
+              <IoChatbubbleOutline size="20" />
+              <span>248</span>
+            </div>
+            <div className="flex gap-1 items-center cursor-pointer hover:bg-amber-200 rounded-4xl p-2">
+              <AiOutlineRetweet size="20" />
+              <span>248</span>
+            </div>
+
+            <div className="flex gap-1 items-center cursor-pointer hover:bg-amber-200 rounded-4xl p-2">
+              <FaRegHeart size="20" />
+              <span>250</span>
+            </div>
+
+            <div className="flex gap-1 items-center cursor-pointer hover:bg-amber-200 rounded-4xl p-2">
+              <IoStatsChartOutline size="20" />
+              <span>72</span>
+            </div>
+          </div>
+
+          <div className="flex justify-around flex-1">
+            <div className="cursor-pointer hover:bg-amber-200 rounded-4xl p-2">
+              <CiBookmark size="20" />
+            </div>
+            <div className="cursor-pointer hover:bg-amber-200 rounded-4xl p-2">
+              <MdOutlineFileUpload size="20" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
